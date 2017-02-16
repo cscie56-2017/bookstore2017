e86029a081b72f1a9fafee3eac3333f9cff3eed1
Michael Kerry <mkerry@fas.harvard.edu>
2017-02-15 21:22:14 -0500

    added named query; toString

--- grails-app/domain/cscie56/demo/Book.groovy
+++ grails-app/domain/cscie56/demo/Book.groovy
@@ -8,7 +8,20 @@ class Book {
 
     static belongsTo = [author:Author]
 
+    static namedQueries = {
+        findAllBooksByPublicationYear { Integer year ->
+            Date start = Date.parse("MM/dd/yyyy","1/1/${year}")
+            Date end = Date.parse("MM/dd/yyyy","1/1/${year+1}")
+            ge 'dateOfPublication', start
+            lt 'dateOfPublication', end
+        }
+    }
+
     static constraints = {
         isbn unique: true
+    }
+
+    String toString(){
+        "$title (${dateOfPublication.format('yyyy')})"
     }
 }
