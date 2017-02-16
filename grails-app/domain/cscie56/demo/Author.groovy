70a271cbdcaa5b4528f8e28e1ff97e386062560f
Michael Kerry <mkerry@fas.harvard.edu>
2017-02-15 21:21:47 -0500

    added middle name; transient fullName; toString

--- grails-app/domain/cscie56/demo/Author.groovy
+++ grails-app/domain/cscie56/demo/Author.groovy
@@ -3,11 +3,26 @@ package cscie56.demo
 class Author {
 
     String firstName
+    String middleName
     String lastName
     Date birthDate
 
     static hasMany = [books:Book]
 
+    static transients = ['fullName']
+
+    String getFullName() {
+        "$firstName ${middleName?middleName+' ':''}$lastName"
+    }
+
     static constraints = {
+        firstName()
+        middleName nullable: true
+        lastName()
+        birthDate()
+    }
+
+    String toString(){
+        fullName
     }
 }
