9fbf2fc1b458db908b9357000571a87b979561b3
mpkerry <mkerry@g.harvard.edu>
2017-02-16 19:26:21 -0500

    adding books

--- src/test/groovy/cscie56/demo/AuthorSpec.groovy
+++ src/test/groovy/cscie56/demo/AuthorSpec.groovy
@@ -1,5 +1,6 @@
 package cscie56.demo
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Author)
+@Mock(Book)
 class AuthorSpec extends Specification {
 
     def setup() {
@@ -15,8 +17,12 @@ class AuthorSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test create Author with books"() {
+        when:
+            Author a = new Author(firstName: 'Stephen',lastName: 'King',birthDate: new Date())
+            Book b = new Book(title: 'book',dateOfPublication: new Date(), isbn: '1239088',author: a)
+            a.books = [] << b
+        then:
+            a.validate()
     }
 }
