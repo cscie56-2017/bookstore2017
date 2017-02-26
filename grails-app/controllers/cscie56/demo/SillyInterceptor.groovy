40b8bc25476a010b18e942b0b149c4a28c938ad3
mpkerry <mkerry@g.harvard.edu>
2017-02-26 16:03:46 -0500

    lecture 6 exercises - interceptor

---
+++ grails-app/controllers/cscie56/demo/SillyInterceptor.groovy
@@ -0,0 +1,27 @@
+package cscie56.demo
+
+
+class SillyInterceptor {
+
+    List<String> badJokes = ["What's brown and sticky? A stick.",
+                     "Why do gorillas have big nostrils? Because gorillas have big fingers.",
+                     "What sound does a nut make when it sneezes? Cashew!",
+                     "Two windmills are on a date and one asks the other, \"So what kind of music do you like?\" The other replies, \"I'm a big metal fan!\"",
+                     "What do you call someone else's cheese? Nacho Cheese!",
+                      null,null,null,null,null]
+
+    public SillyInterceptor() {
+        match controller:'book'
+    }
+
+    boolean before() {
+        flash.message = badJokes.get(new Random().nextInt(10))
+        true
+    }
+
+    boolean after() { true }
+
+    void afterView() {
+        // no-op
+    }
+}
---
+++ src/test/groovy/cscie56/demo/SillyInterceptorSpec.groovy
@@ -0,0 +1,37 @@
+package cscie56.demo
+
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(SillyInterceptor)
+class SillyInterceptorSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+
+    }
+
+    void "Test silly interceptor matching"() {
+        when:"A request matches the interceptor"
+            withRequest(controller:"book")
+
+        then:"The interceptor does match"
+            interceptor.doesMatch()
+    }
+
+    void "Test whether joke is there" () {
+        when: "A request matches the interceptor"
+            withRequest(controller:"book")
+            interceptor.before()
+        then:
+            if (interceptor?.flash?.message) {
+                interceptor?.badJokes?.contains(interceptor.flash.message)
+            }
+    }
+}
