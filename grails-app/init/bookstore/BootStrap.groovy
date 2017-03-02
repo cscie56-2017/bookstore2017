76f3ceccfb936f3ca8547805770f0f761749b64e
mpkerry <mkerry@g.harvard.edu>
2017-03-01 20:58:52 -0500

    lecture 6 exercises - revised for new domain fields

--- grails-app/init/bookstore/BootStrap.groovy
+++ grails-app/init/bookstore/BootStrap.groovy
@@ -16,30 +16,31 @@ class BootStrap {
         Author a1 = new Author(firstName: 'Stephen', lastName: 'King', birthDate: Date.parse("MM/dd/yyyy","9/21/1947"))
         saveObject(a1)
 
-        saveBook('The Shining', "1/27/1977" , [a1], '978-0-385-12167-5',p1,1999)
-        saveBook("Salem's Lot", "10/17/1975", [a1], '978-0-385-00751-1',p1,2499)
-        saveBook("Carrie",        "4/5/1974", [a1], '978-0-385-08695-0',p1,2999)
-        saveBook("The Dead Zone", "8/1/1979", [a1], '978-0-670-26077-5',p1,2999)
+        saveBook('The Shining', "1/27/1977" , [a1], '978-0-385-12167-5',p1,1999,'Horror')
+        saveBook("Salem's Lot", "10/17/1975", [a1], '978-0-385-00751-1',p1,2499,'Horror')
+        saveBook("Carrie",        "4/5/1974", [a1], '978-0-385-08695-0',p1,2999,'Horror')
+        saveBook("The Dead Zone", "8/1/1979", [a1], '978-0-670-26077-5',p1,2999,'Horror')
 
         Author a2 = new Author(firstName: 'Patrick', lastName: "O'Brian", birthDate: Date.parse("MM/dd/yyyy","12/12/1914"))
         saveObject(a2)
 
-        saveBook('Master and Commander',    "1/1/1969", [a2], '0-00-221526-8',p2, 999)
-        saveBook("Post Captain",            "1/1/1972", [a2], '0-00-221657-4',p2,1299)
-        saveBook("HMS Surprise",            "1/1/1973", [a2], '0-397-00998-4',p2,1999)
-        saveBook("The Mauritius Command ",  "1/1/1977", [a2], '0-00-222383-X',p2,1999)
+        saveBook('Master and Commander',    "1/1/1969", [a2], '0-00-221526-8',p2, 999, 'Drama')
+        saveBook("Post Captain",            "1/1/1972", [a2], '0-00-221657-4',p2,1299, 'Drama')
+        saveBook("HMS Surprise",            "1/1/1973", [a2], '0-397-00998-4',p2,1999, 'Drama')
+        saveBook("The Mauritius Command ",  "1/1/1977", [a2], '0-00-222383-X',p2,1999, 'Drama')
 
     }
     def destroy = {
     }
 
-    def saveBook(title,dateString,authors,isbn,publisher,price) {
+    def saveBook(title,dateString,authors,isbn,publisher,price,genre) {
         Book b = new Book(title:title,
                           dateOfPublication: Date.parse("MM/dd/yyyy",dateString),
                           authors:authors,
                           isbn:isbn,
                           publisher: publisher,
-                          price:price)
+                          price:price,
+                          genre: genre)
         saveObject(b)
         authors.each { author ->
             author.addToBooks(b)
